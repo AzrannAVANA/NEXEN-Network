36789570ce0dc06b16e813190b78f769358b0e46
Azrann AVANA <112041895+AzrannAVANA@users.noreply.github.com>
2025-04-22 15:14:03 +0200

    Add latest known version of previous Corium Project

---
+++ index.php
@@ -0,0 +1,72 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="description" content="Description du projet en groupe">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+    <title>Corium - Core Forums</title>
+    <link href="./style.css" rel="stylesheet">
+    <script src="https://kit.fontawesome.com/406da18a0b.js" crossorigin="anonymous" defer></script>
+</head>
+<body>
+    <header class="WhiteBands">
+        <div id="Header_Home">
+            <nav id="Header_Navigation">
+                <div>
+                    <i id="Header_CoriumLogo"></i>
+                </div>
+                <h1>Corium</h1>
+                <p>Core Forums. A Project</p>
+            </nav>
+        </div>
+    </header>
+    <main >
+        <section id="MainGrid">
+            <a id="Test_Core3" class="Core Core_2x2">
+                <p>
+                    A Core's Test <br>
+                    Dog
+                </p>
+            </a>
+
+            <a id="Test_Core4" class="Core Core_2x1">
+                <p>
+                    A Core's Test <br>
+                    Jacksmith
+                </p>
+            </a>
+
+            <a id="Test_Core1" class="Core Core_1x1">
+                <p>
+                    A Core's Test <br>
+                    FUSION
+                </p>
+            </a>
+
+            <a id="Test_Core5" class="Core Core_1x2">
+                <p>
+                    THE NEWS <br>
+                    this is weird
+                </p>
+            </a>
+
+            <a id="Test_Core6" class="Core Core_2x1">
+            </a>
+
+            <a id="Test_Core2" class="Core Core_1x1">
+                <p>
+                    A Core's Test <br>
+                    Forge Of Empires
+                </p>
+                <div >
+                    <img id="Core2_IMG" src="./img/articles/forge-of-empire-banner.jpg.webp" alt="Gaming"></img>
+                </div>
+            </a>
+        </section>
+    </main>
+    <footer class="WhiteBands">
+        <p>Footer</p>
+    </footer>
+</body>
+</html>
